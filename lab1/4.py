6b7b35cd6324a1072a9d24b5432f7e7316fa587c
Ansar <ansar.sanchez@gmail.com>
2025-01-20 23:44:00 +0500

    Python Comments

--- lab1/4.py
+++ lab1/4.py
@@ -8,3 +8,10 @@ print("Hello, World!") #This is a comment
 #Example 3
 #print("Hello, World!")
 print("Cheers, Mate!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
